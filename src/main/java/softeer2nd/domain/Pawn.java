963f00a0bc24f40976d8d38e7c1aadd2dbe9fe58
plusjob70 <plusjob70@naver.com>
2023-07-03 17:51:03 +0900

    test(Pawn, PawnTest): 미션 1 - 폰 생성
    2. Pawn 클래스 구현

---
+++ src/main/java/softeer2nd/domain/Pawn.java
@@ -0,0 +1,15 @@
+package softeer2nd.domain;
+
+public class Pawn {
+
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
--- src/test/java/softeer2nd/domain/PawnTest.java
+++ src/test/java/softeer2nd/domain/PawnTest.java
@@ -8,10 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다.")
+    @DisplayName("폰이 생성되어야 한다.")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn pawn1 = new Pawn("white");
+        assertThat(pawn1.getColor()).isEqualTo("white");
+
+        Pawn pawn2 = new Pawn("black");
+        assertThat(pawn2.getColor()).isEqualTo("black");
     }
 
 }
