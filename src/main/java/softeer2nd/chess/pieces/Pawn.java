191b5314c224eedd150049c7be3303b54f3ff082
plusjob70 <plusjob70@naver.com>
2023-07-04 12:18:40 +0900

    feat(Pawn, PawnTest): Pawn 색에 따른 출력 문자 부여
    - 흰색은 소문자 p, 검은색은 대문자 P
    - 생성자의 인자를 하나 더 받아서 출력 문자를 부여하도록 구현

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -4,19 +4,33 @@ public class Pawn {
 
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
 
     private final String color;
+    private final char representation;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
     public Pawn(String color) {
         this.color = color;
+        this.representation = color.equals(WHITE_COLOR) ? WHITE_REPRESENTATION : BLACK_REPRESENTATION;
+    }
+
+    public Pawn(String color, char representation) {
+        this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 
 }
