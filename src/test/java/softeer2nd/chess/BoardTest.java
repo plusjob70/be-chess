d4f00ef6deed0d227a3bfcaf7e331f57fd750b0a
plusjob70 <plusjob70@naver.com>
2023-07-04 11:27:30 +0900

    test(BoardTest): 중복 제거
    - 보드에 Pawn 이외의 객체가 추가될 때 발생하는 컴파일 오류를 확인한다.
    - 보드의 범위 밖에서 Pawn을 찾으려고 하면
    예외가 발생한다.

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
@@ -9,26 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BoardTest {
 
+    private Board board;
+
+    @BeforeEach
+    public void createBoard() {
+        board = new Board();
+    }
+
     @Test
-    public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+    @DisplayName("보드에 Pawn을 추가한다.")
+    public void addPawn() {
+        verifyAdd(new Pawn(Pawn.WHITE_COLOR), 1, 0);
+        verifyAdd(new Pawn(Pawn.BLACK_COLOR), 2, 1);
     }
 
     @Test
     @DisplayName("보드에는 Pawn 객체만 추가될 수 있다.")
     public void addOtherObject() {
-        Board board = new Board();
-
         // 컴파일 에러 발생
         // board.add(new Integer("7"));
     }
@@ -36,12 +34,17 @@ class BoardTest {
     @Test
     @DisplayName("보드의 범위 밖에서 Pawn을 찾을 때 예외가 발생한다.")
     public void findPawnOutOfBounds() {
-        Board board = new Board();
         board.add(new Pawn());
 
         assertThrows(IndexOutOfBoundsException.class,
                 () -> board.findPawn(10)
         );
+    }
+
+    private void verifyAdd(Pawn pawn, int expectedBoardSize, int pawnIndex) {
+        board.add(pawn);
+        assertEquals(expectedBoardSize, board.size());
+        assertEquals(pawn, board.findPawn(pawnIndex));
     }
 
 }
