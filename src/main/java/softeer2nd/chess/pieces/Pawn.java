df5c325b8fc89ad4eb05eff3ffbcaed37f8d9bf4
plusjob70 <plusjob70@naver.com>
2023-07-04 17:22:43 +0900

    refactor(Pawn): Pawn 기본 생성자 변경
    Pawn의 기본 생성자가 내부에서 다른 생성자를 호출하는 형식으로 변경

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -11,8 +11,7 @@ public class Pawn {
     private final char representation;
 
     public Pawn() {
-        this.color = WHITE_COLOR;
-        this.representation = WHITE_REPRESENTATION;
+        this(WHITE_COLOR);
     }
 
     public Pawn(String color) {
