81e8ac5a3d7844136f802de773d71d927532290c
plusjob70 <plusjob70@naver.com>
2023-07-06 07:52:11 +0900

    refactor(Main): GameManager -> Main 변경

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,7 +1,34 @@
 package softeer2nd;
 
+import softeer2nd.chess.Board;
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static final String START_COMMAND = "start";
+    public static final String END_COMMAND = "end";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String command;
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            while (true) {
+                command = scanner.next();
+
+                if (command.equals(START_COMMAND)) {
+                    board.initialize();
+                    System.out.println(board.showBoard());
+                } else if (command.equals(END_COMMAND)) {
+                    break;
+                }
+            }
+        } catch (Exception ignored) {
+
+        } finally {
+            scanner.close();
+        }
     }
 }
