b2a86e817c9dbd5aa3c510b292f3b88ffe9b74fa
plusjob70 <plusjob70@naver.com>
2023-07-08 13:47:55 +0900

    style(StringUtils): StringUtils 주석 추가

--- src/main/java/softeer2nd/chess/utils/StringUtils.java
+++ src/main/java/softeer2nd/chess/utils/StringUtils.java
@@ -1,18 +1,34 @@
 package softeer2nd.chess.utils;
 
+/**
+ * 문자열 생성 관련 유틸리티
+ */
 public class StringUtils {
-
+    /**
+     * 줄바꿈 문자열
+     */
     public static final String NEWLINE = System.getProperty("line.separator");
 
+    /**
+     * 생성자를 통해서 해당 인스턴스를 생성할 수 없다.
+     */
     private StringUtils() {
     }
 
+    /**
+     * 인자로 받은 문자열에 줄바꿈하여 반환한다.
+     * @param string 문자열
+     * @return 줄바꿈된 문자열
+     */
     public static String appendNewLine(String string) {
         return string + NEWLINE;
     }
 
+    /**
+     * StringBuilder 객체에 줄바꿈을 추가한다.
+     * @param stringBuilder
+     */
     public static void appendNewLine(StringBuilder stringBuilder) {
         stringBuilder.append(NEWLINE);
     }
-
 }
