f0a6ffd9f53562c7923f30f1c265b418f082616b
plusjob70 <plusjob70@naver.com>
2023-07-09 17:12:38 +0900

    style(Position): 주석 추가 및 코드 스타일 변경

--- src/main/java/softeer2nd/chess/Position.java
+++ src/main/java/softeer2nd/chess/Position.java
@@ -29,26 +29,68 @@ public class Position {
         this.y = y;
     }
 
-    /**
-     * Position 인스턴스 생성 팩토리 메서드
-     * @param expression 체스 보드판의 위치 좌표
-     * @return Position
-     */
-    public static Position create(String expression) {
-        if (!expression.matches(LEGAL_EXPRESSION_POSITION)) {
-            throw new IllegalArgumentException("잘못된 위치입니다.");
-        }
-        int x = Board.SIZE - Character.getNumericValue(expression.charAt(1));
-        int y = expression.charAt(0) - 'a';
-        return new Position(x, y);
-    }
-
     public int getX() {
         return x;
     }
 
     public int getY() {
         return y;
+    }
+
+    /**
+     * Position 인스턴스 생성 팩토리 메서드
+     * @param expression 체스 보드판의 위치 좌표
+     * @return Position
+     */
+    public static Position create(String expression) {
+        validateExpression(expression);
+        int x = Board.SIZE - Character.getNumericValue(expression.charAt(1));
+        int y = expression.charAt(0) - 'a';
+        return new Position(x, y);
+    }
+
+    /**
+     * Position 인스턴스 생성 팩토리 메서드
+     * @param x x 좌표
+     * @param y y 좌표
+     * @return Position
+     */
+    public static Position create(int x, int y) {
+        validateIndex(x, y);
+        return new Position(x, y);
+    }
+
+    /**
+     * x좌표, y좌표가 체스 보드의 인덱스에 유효한지 검사
+     * @param x x좌표
+     * @param y y좌표
+     * @return true if valid else false
+     */
+    public static boolean isValid(int x, int y) {
+        return 0 <= x && x < Board.SIZE && 0 <= y && y < Board.SIZE;
+    }
+
+    /**
+     * 체스 보드 위치 표현식 유효성 검사
+     * @param expression 표현식
+     * @throws IllegalArgumentException 위치 표현식이 아니거나 범위를 벗어날 때
+     */
+    private static void validateExpression(String expression) {
+        if (!expression.matches(LEGAL_EXPRESSION_POSITION)) {
+            throw new IllegalArgumentException("잘못된 위치입니다.");
+        }
+    }
+
+    /**
+     * 체스 보드 인덱스 유효성 검사
+     * @param x x 좌표
+     * @param y y 좌표
+     * @throws IllegalArgumentException 인덱스가 범위를 벗어날 때
+     */
+    private static void validateIndex(int x, int y) {
+        if (!isValid(x, y)) {
+            throw new IllegalArgumentException("잘못된 위치입니다.");
+        }
     }
 
     @Override
